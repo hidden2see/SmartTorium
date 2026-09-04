98d4095b7445bbbb1457dad898cf44450bf4e6e5
Hidden2See <119127175+hidden2see@users.noreply.github.com>
2026-09-04 09:42:45 +0300

    build: add region_endpoint.py
    Added region_endpoint.py that contain the NewEndPoint class with the porpuse of helping the user change where Tor Browser will connect

---
+++ scripts/region_endpoint.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import shutil
+import pycountry
+from time import sleep
+
+class NewEndPoint:
+    def __init__(self):
+        self.path = ""
+        self.region = ""
+        self.errors = False
+
+    def FindTorrcPath(self):
+        initial_paths = list(Path("/").rglob("torrc"))
+        file_found = False
+
+        for ipath in initial_paths:
+            for search_file in ipath.parent.iterdir():
+                if search_file.name == "torrc-defaults":
+                    file_found = True
+                    self.path = ipath
+                    break
+            if file_found:
+                break
+
+        if not file_found:
+            raise KeyError("The program was unable to find the Torrc file, ensure Tor Browser is installed correctly. If Tor Browser is installed contact support.")
+
+    def RefreshTorrc(self):
+        shutil.copyfile(self.path.with_name("torrc-defaults"), self.path)
+
+    def RequestContry(self):
+        code = input("Enter the country code you wanna use for your end point connection: ")
+        cleaned_code = code.strip().upper()
+
+        if pycountry.countries.get(alpha_2=cleaned_code):
+            self.region = cleaned_code
+        elif pycountry.countries.get(alpha_3=cleaned_code):
+            country = pycountry.countries.get(alpha_3=cleaned_code)
+            self.region = country.alpha_2.upper()
+        else:
+            print("Please enter a valid country code")
+            sleep(5)
+            self.RequestContry()
+
+    def AddTheContry(self):
+        with open(self.path, "a") as file:
+            file.write(f'''
+                ExitNodes {{{self.region}}}
+                StrictNodes 1
+            ''')
